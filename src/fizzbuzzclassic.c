684076ca3207a51f56ab3f024b866d8d21c97723
Daniel Lewis <thecontrarian42@gmail.com>
2012-07-12 22:40:28 -0500

    weird problems with git.
    but whatever. this is what the commit "classic fizzbuzz" should have
    been.

--- src/fizzbuzzclassic.c
+++ src/fizzbuzzclassic.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+//This program prints to the console the numbers 1 through
+//100, with the exception of those numbers evenly divisible
+//by 3, 5, or both 3 and 5, for which it prints the words
+//"Fizz", "Buzz", or "FizzBuzz" respectively.
+
 int main(){
+	char *five = "Buzz";
+	char *three = "Fizz";
+	char *both = "FizzBuzz";
+	for (int i = 1; i <= 100; ++i)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				printf("%s\n", both);
+				continue;
+			}
+			printf("%s\n", three);
+			continue;
+		}
+		if (i % 5 == 0)
+		{
+			printf("%s\n", five);
+			continue;
+		}
+		printf("%d\n", i);
+		continue;
+	}
 
 	return 0;
 }
