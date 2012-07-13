cdc28cf14fc2ff37805a41dd9b190437606bdcb2
Daniel Lewis <thecontrarian42@gmail.com>
2012-07-13 03:01:30 -0500

    moved from Xcode to just gcc and a text editor
    Blugh Xcode

---
+++ FizzBuzzLib/fizzbuzzlib.h
@@ -0,0 +1,14 @@
+//
+//  fizzbuzzlib.h
+//  FizzBuzzLib
+//
+//  Created by Daniel Lewis on 7/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef FizzBuzzLib_fizzbuzzlib_h
+#define FizzBuzzLib_fizzbuzzlib_h
+
+
+
+#endif
